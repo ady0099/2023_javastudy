f893dc255eb8a0422efa149cb0355c7614ab6841
ady <ady9011@naver.com>
2023-05-23 16:21:28 +0900

    두 번 째

--- src/Homework/Hw0524_Event.java
+++ src/Homework/Hw0524_Event.java
@@ -24,7 +24,6 @@ import javax.swing.ScrollPaneConstants;
 // jta
 // 계산 종료 초기화
 
-
 public class Hw0524_Event extends JFrame implements ActionListener, ItemListener {
 	JPanel jp1, jp2;
 	JTextField jtf1, jtf2;
@@ -33,123 +32,138 @@ public class Hw0524_Event extends JFrame implements ActionListener, ItemListener
 	JTextArea jta;
 	JScrollPane jsp;
 	JButton calc, exit, reset;
-	
+
 	public Hw0524_Event() {
 		super("계산기");
 
 		jp1 = new JPanel();
 		jtf1 = new JTextField(5);
 		jtf2 = new JTextField(5);
-	
+
 		jp1.add(new JLabel("수1 "));
 		jp1.add(jtf1);
 		jp1.add(new JLabel("수2 "));
 		jp1.add(jtf2);
-		
+
 		jp1.add(new JLabel("연산자 : "));
 		jrb1 = new JRadioButton(" + ");
 		jrb2 = new JRadioButton(" - ");
 		jrb3 = new JRadioButton(" * ");
 		jrb4 = new JRadioButton(" / ");
-		
+
 		jp1.add(jrb1);
 		jp1.add(jrb2);
 		jp1.add(jrb3);
 		jp1.add(jrb4);
-		
+
 		bg = new ButtonGroup();
 		bg.add(jrb1);
 		bg.add(jrb2);
 		bg.add(jrb3);
 		bg.add(jrb4);
-		
+
 		jta = new JTextArea(10, 10);
-		jsp = new JScrollPane(jta,
-				ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+		jsp = new JScrollPane(jta, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
 				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		jta.setLineWrap(true);
 		jta.setEditable(false);
-		
-		
+
 		jp2 = new JPanel();
-		
+
 		calc = new JButton("계산");
 		exit = new JButton("종료");
 		reset = new JButton("초기화");
-		
+
 		jp2.add(calc);
 		jp2.add(exit);
 		jp2.add(reset);
-		
-	
-		
+
 		add(jp1, BorderLayout.NORTH);
 		add(jsp, BorderLayout.CENTER);
 		add(jp2, BorderLayout.SOUTH);
-		
-		
+
 		pack();
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
-		
+
 		jrb1.addItemListener(this);
 		jrb2.addItemListener(this);
 		jrb3.addItemListener(this);
 		jrb4.addItemListener(this);
-		
+
 		calc.addActionListener(this);
 		exit.addActionListener(this);
 		reset.addActionListener(this);
-		
+
 	}
+
 	@Override
-	public void itemStateChanged(ItemEvent e) {		
+	public void itemStateChanged(ItemEvent e) {
 		double d1, d2, d3;
-		
-		JRadioButton obj = (JRadioButton)e.getSource();
-		if(e.getStateChange() == e.SELECTED) {
-	
+
+		JRadioButton obj = (JRadioButton) e.getSource();
+		if (e.getStateChange() == e.SELECTED) {
+
 		}
-		
+
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		JButton obj = (JButton)e.getSource();
-		
-		
-		if(obj == exit) {
+		JButton obj = (JButton) e.getSource();
+
+		if (obj == exit) {
 			System.out.println("프로그램 종료");
 			System.exit(0);
-		}else if(obj == reset) {
+		} else if (obj == reset) {
 			bg.clearSelection();
 			jtf1.setText("");
 			jtf1.requestFocus();
 			jta.setText("");
 			System.out.println("값 초기화");
-		}else if(obj == calc) {
+		} else if (obj == calc) {
 			calc();
-			
+
 		}
 
 	}
 
 	void calc() {
 		String regX = "[0-9]+";
-		
-		JRadioButton obj = (JRadioButton)e.getSource();
-		
+
 		String a = jtf1.getText();
 		String b = jtf2.getText();
-		int par = Integer.parseInt(a)+Integer.parseInt(b);
-		
-		if(a.matches(regX) || b.matches(regX)) {
-			jta.setText(a + " + " + b +" = "+par);
-			System.out.println("calc() 실행.. 값 : "+par);
+		String op = "";
+
+		int par = 0;
+		double par2 = 0.0;
+
+		if (a.matches(regX) || b.matches(regX)) {
+			if (jrb1.isSelected()) {
+				op=jrb1.getText();
+				par = Integer.parseInt(a) + Integer.parseInt(b);
+				jta.setText(a + " "+ op + " " + b + "  =  " + par);
+				System.out.println("calc() 실행.. [" + op+ "]연산.. 값 : "+ par);
+
+			}else if(jrb2.isSelected()) {
+				op=jrb2.getText();
+				par = Integer.parseInt(a) - Integer.parseInt(b);
+				jta.setText(a + " "+ op + " " + b + "  =  " + par);
+				System.out.println("calc() 실행.. [" + op+"]연산.. 값 : " + par);
+			}
+
+		} else {
+			try {
+
+			} catch (Exception e) {
+				jta.setText("연산자를 선택해주세요.");
+			}
+
 		}
+
 	}
-	
+
 	public static void main(String[] args) {
 		new Hw0524_Event();
 	}
